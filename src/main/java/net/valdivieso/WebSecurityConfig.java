e3212010cfa60eb170e03b38f6ac0c6da6bf8c53
Richard Valdivieso <richard@valdivieso.net>
2017-05-27 14:04:57 -0400

    Adding Configuration annotation

--- src/main/java/net/valdivieso/WebSecurityConfig.java
+++ src/main/java/net/valdivieso/WebSecurityConfig.java
@@ -1,12 +1,14 @@
 package net.valdivieso;
 
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
